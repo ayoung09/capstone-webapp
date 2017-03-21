89ee72521b9d7d44c5e54eed4848663ebaf73f69
Alexia Young <alexia.kwok@gmail.com>
2017-03-21 17:07:19 -0500

    abstracted out utility functions in wait for captions

--- browser/components/DrawkwardWaitForCaptions.jsx
+++ browser/components/DrawkwardWaitForCaptions.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
-import socket from '../socket';
 import shuffle from 'shuffle-array';
+import socket from '../socket';
 
 import DrawkwardShowDrawing from './DrawkwardShowDrawing';
 import { setCurrentDrawing, addPhraseGuess } from '../reducers/drawkwardRound';
@@ -27,7 +27,7 @@ class DrawkwardWaitForCaptions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startCaptionSent: false,
+      startCaptionEmitted: false,
     };
   }
 
@@ -35,35 +35,47 @@ class DrawkwardWaitForCaptions extends React.Component {
     this.props.setCurrentDrawing();
 
     socket.on(receiveNewGuess, userObj => {
-      console.log('userobj', userObj)
       this.props.addPhraseGuess(userObj.id, userObj.guessString);
     });
-  }
-
-  componentWillUnmount() {
-    socket.off(receiveNewGuess);
   }
 
   componentWillReceiveProps(nextProps) {
     let currentArtist = nextProps.currentDrawing.id;
     let usersToReceive = Object.keys(this.props.users).filter(user => user !== currentArtist);
 
-    if (!this.state.startCaptionSent) {
+    if (!this.state.startCaptionEmitted) {
       socket.emit(sendToArtist, currentArtist);
       socket.emit(sendStartCaption, usersToReceive);
-      this.setState({startCaptionSent: true});
+      this.setState({startCaptionEmitted: true});
     }
 
-    if (nextProps.phraseGuesses.length === this.props.numOfUsers - 1) {
-      let captionArray = nextProps.phraseGuesses.map(phraseObj => {
+    const allUsersHaveSubmittedPhraseGuesses = () => {
+      return nextProps.phraseGuesses.length === this.props.numOfUsers - 1;
+    };
+
+    const includeOriginalPhrase = (captionGuessesArr) => {
+      captionGuessesArr.push(this.props.currentDrawing.drawingObj.phrase);
+      return captionGuessesArr;
+    };
+
+    const buildAndShuffleCaptionArr = () => {
+      let captionGuessesArray = nextProps.phraseGuesses.map(phraseObj => {
           return Object.keys(phraseObj)[0];
       });
-      captionArray.push(this.props.currentDrawing.drawingObj.phrase);
-      captionArray = shuffle(captionArray);
+      let includingOriginalPhrase = includeOriginalPhrase(captionGuessesArray);
+      let finalCaptionArray = shuffle(includingOriginalPhrase);
+      return finalCaptionArray;
+    };
 
-      socket.emit(receivedAllCaptions, {usersToReceive, captionArray});
+    if (allUsersHaveSubmittedPhraseGuesses()) {
+      let captionArrayToSend = buildAndShuffleCaptionArr();
+      socket.emit(receivedAllCaptions, {usersToReceive, captionArrayToSend});
       browserHistory.push('/drawkward/listCaptions');
     }
+  }
+
+  componentWillUnmount() {
+    socket.off(receiveNewGuess);
   }
 
   render() {
