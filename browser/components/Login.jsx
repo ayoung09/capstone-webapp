6d7619c2f726063debcbd0926f0c797a555e6cc1
Alexia Young <alexia.kwok@gmail.com>
2017-03-23 18:01:18 -0500

    random room generator working on login component

--- browser/components/Login.jsx
+++ browser/components/Login.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-const Login = () => {
-  return (
-    <div>
-      <h2>Choose a game to play!</h2>
-      <Link to="/drawkward"><button className="btn-game">Drawkward</button></Link>
-      <Link to="/pictionary"><button className="btn-game">Pictionary</button></Link>
-    </div>
-  );
-};
+//const mapStateToProps =
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      roomName: '',
+    };
+    this.onGamePress = this.onGamePress.bind(this);
+  }
+
+  generateRandomRoomName() {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const roomNameLength = 4;
+    let roomName = '';
+    while (roomName.length < roomNameLength) {
+      let randomLetter = alphabet[Math.ceil(Math.random() * 25)];
+      roomName += randomLetter;
+    }
+    return roomName;
+  }
+
+  onGamePress(e) {
+    console.log('this is in onGamePress: ', this.props.roomName, 'and this is in params: ', e.target);
+  }
+
+  render() {
+    console.log('this is roomName: ', this.generateRandomRoomName());
+    return (
+      <div>
+        <h2>Create a name for your Game Room and then select a Game to play</h2>
+        <form>
+          <h1 className="room-form-header">{this.props.roomName}</h1>
+          <br />
+          <Link to="/drawkward"><button onClick={this.onGamePress}className="btn-game">Drawkward</button></Link>
+          <Link to="/pictionary"><button type="submit" onClick={this.onGamePress}className="btn-game">Pictionary</button></Link>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
