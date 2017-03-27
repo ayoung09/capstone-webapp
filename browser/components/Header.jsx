c3cb29e4a6c13704d404b3e87da4b2c7777aa3aa
Alexia Young <alexia.kwok@gmail.com>
2017-03-26 20:43:34 -0500

    created header component

---
+++ browser/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img id="drawkward-logo" src="/images/drawkward.png" />
+    </div>
+  );
+};
+
+export default Header;
